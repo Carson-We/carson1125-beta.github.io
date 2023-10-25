17fb661b57fcc0fcbc69b0e88586af946cf09977
Carson-We <115160605+Carson-We@users.noreply.github.com>
2023-10-25 22:03:35 +0800

    Add files via upload

---
+++ scripts/healthcheck.js
@@ -0,0 +1,28 @@
+<script>
+    var websiteUrl = "http://carson1125.hk";
+
+       function checkWebsiteStatus() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", websiteUrl, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    updateWebsiteStatus("Normal", "#32CD32");
+                } else {
+                    updateWebsiteStatus("Offline", "#FF0000");
+                }
+            }
+        };
+        xhr.send();
+    }
+
+    function updateWebsiteStatus(status, color) {
+        var statusElement = document.getElementById("website-status");
+        statusElement.textContent = "Server: " + status;
+        statusElement.style.color = color;
+    }
+
+    window.addEventListener('load', function () {
+        checkWebsiteStatus();
+    });
+</script>
