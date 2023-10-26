a9794a12f31b47d5947b6461669c39336225480e
Carson-We <115160605+Carson-We@users.noreply.github.com>
2023-10-26 22:00:59 +0800

    V 0.4

---
+++ scripts/nor_kr.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', function() {
+            document.body.classList.add('loaded');
+        });
+
+        function goToHomePage() {
+            window.location.href = "index_kr.html";
+        }
+
+        function goToCR() {
+            window.location.href = "CR_kr.html";
+        }
+
+        window.addEventListener('load', function() {
+            document.body.classList.add('loaded');
+        });
+
+        window.addEventListener('DOMContentLoaded', function() {
+            var dropdownButton = document.querySelector('.dropbtn');
+            var dropdownContent = document.querySelector('.dropdown-content');
+
+            var isHovered = false;
+
+            dropdownButton.addEventListener('click', function() {
+                dropdownContent.classList.toggle('active');
+                isHovered = false;
+            });
+            
+            window.addEventListener('click', function(event) {
+                if (!dropdownButton.contains(event.target)) {
+                    dropdownContent.classList.remove('active');
+                    isHovered = false;
+                }
+            });
+        });
