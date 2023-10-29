1ec09a7bf3624091cc78491174b69b956d208ad2
Carson-We <115160605+Carson-We@users.noreply.github.com>
2023-10-29 21:39:16 +0800

    V 0.5.2 ~ V 0.6.1

---
+++ scripts/healthcheck_kr.js
@@ -0,0 +1,26 @@
+var websiteUrl = "http://carson1125.hk";
+
+       function checkWebsiteStatus() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", websiteUrl, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    updateWebsiteStatus("정상", "#32CD32");
+                } else {
+                    updateWebsiteStatus("오프라인", "#FF0000");
+                }
+            }
+        };
+        xhr.send();
+    }
+
+    function updateWebsiteStatus(status, color) {
+        var statusElement = document.getElementById("website-status");
+        statusElement.textContent = "Server: " + status;
+        statusElement.style.color = color;
+    }
+
+    window.addEventListener('load', function () {
+        checkWebsiteStatus();
+    });
